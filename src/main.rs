079fb7ec3bd14853ed81db607cea634ccad816d3
Popochounet <adrien.audiard@epitech.eu>
2026-05-23 14:11:51 +0200

    feat(main): final pipeline + display tree

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 mod input;
-mod slr;
 mod ruleparser;
+mod slr;
 
+use crate::{
+    input::get_rule_and_input,
+    ruleparser::{reader::parse_rules, structs::TokenManager},
+    slr::{parser::TokenWithMetadata, tree::TreeNode},
+};
 
-use crate::{input::get_rule_and_input, ruleparser::{reader::parse_rules}};
+fn display_node(node: &TreeNode<TokenWithMetadata>, depth: usize, token_manager: &TokenManager) {
+    println!("{}", token_manager.get_token_name(node.value.token.id()));
+    for child in &node.children {
+        print!("{}   \\--- ", "\t".repeat(depth),);
+        display_node(child, depth + 1, token_manager);
+    }
+}
 
 fn main() {
-    let mut rules = ruleparser::structs::TokenManager::new();
     let input = get_rule_and_input();
+
+    let mut rules = ruleparser::structs::TokenManager::new();
     rules.add_productions(&parse_rules(&input.rules));
-    let parser = slr::parser::LRTable::new(&rules.get_production());
-    println!("input tokens: {}", input.input_tokens);
-    println!("{}", parser);
-    println!("{}", rules);
-    // println!("{:?}", rules);
+
+    let tokens = rules.scan_tokens(&input.input_tokens);
+
+    let mut parser = slr::parser::Parser::new(&rules.get_production());
+
+    match parser.parse(tokens) {
+        Ok(tree) => display_node(&tree.root, 0, &rules),
+        Err(e) => println!("{}", e),
+    }
 }
